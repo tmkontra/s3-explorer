509d0d579ca16532030bd55e43fb975161134f6b
Tyler M Kontra <tyler@tylerkontra.com>
2022-07-18 23:29:05 -0700

    aws sdk v2

--- filesystem/s3_filesystem.go
+++ filesystem/s3_filesystem.go
@@ -2,51 +2,59 @@ package filesystem
 
 import (
 	"bytes"
-	"fmt"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"context"
+	"errors"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 )
 
 type S3Filesystem struct {
 	bucketName   string
 	bucketRegion string
-	session      *session.Session
+	config       aws.Config
+	context      context.Context
 }
 
 func NewS3Filesystem(name string, region string) S3Filesystem {
 	// Loads AWS credentials from environment
-	awsSession := session.Must(session.NewSession())
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		panic(err)
+	}
 	return S3Filesystem{
 		bucketName:   name,
 		bucketRegion: region,
-		session:      awsSession,
+		config:       cfg,
+		context:      context.TODO(),
 	}
 }
 
 func (fs S3Filesystem) GetPath(path string) (Result, error) {
-	s3Client := s3.New(fs.session)
+	s3Client := s3.NewFromConfig(fs.config)
 	// First try to get the key
 	result, err := fs.getFile(s3Client, path)
 	if err != nil {
 		// If there is no object at the key, treat it like a "folder"
-		if aerr, ok := err.(awserr.Error); ok && aerr.Code() == s3.ErrCodeNoSuchKey {
-			// TODO: return "error: not found" when no files at this folder path?
-			return getFolder(s3Client, &fs.bucketName, &path)
+		var nsk *types.NoSuchKey
+		if errors.As(err, &nsk) {
+			// TODO: should return "error: not found" when no files at this folder path prefix?
+			return fs.getFolder(s3Client, &fs.bucketName, &path)
 		} else { // Other error codes are fatal errors
-			return nil, fmt.Errorf(aerr.Message())
+			return nil, err
 		}
 	}
 	// Object was found at the key
 	return result, nil
 }
 
-func (fs S3Filesystem) getFile(s3Client *s3.S3, path string) (Result, error) {
+func (fs S3Filesystem) getFile(s3Client *s3.Client, path string) (Result, error) {
 	input := s3.GetObjectInput{
 		Bucket: &fs.bucketName,
 		Key:    &path,
 	}
-	result, err := s3Client.GetObject(&input)
+	result, err := s3Client.GetObject(fs.context, &input)
 	if err != nil {
 		return nil, err
 	}
@@ -59,12 +67,12 @@ func (fs S3Filesystem) getFile(s3Client *s3.S3, path string) (Result, error) {
 	return FileResult{Path: path, Content: content}, nil
 }
 
-func getFolder(s3Client *s3.S3, bucketName *string, path *string) (Result, error) {
+func (fs S3Filesystem) getFolder(s3Client *s3.Client, bucketName *string, path *string) (Result, error) {
 	listInput := s3.ListObjectsV2Input{
 		Bucket: bucketName,
 		Prefix: path,
 	}
-	listObjectsResult, err := s3Client.ListObjectsV2(&listInput)
+	listObjectsResult, err := s3Client.ListObjectsV2(fs.context, &listInput)
 	if err != nil {
 		return nil, err
 	}
@@ -72,7 +80,7 @@ func getFolder(s3Client *s3.S3, bucketName *string, path *string) (Result, error
 	for _, obj := range listObjectsResult.Contents {
 		children = append(children, ChildResult{
 			Key:  *obj.Key,
-			Size: *obj.Size,
+			Size: obj.Size,
 		})
 	}
 	return FolderResult{Path: *path, Children: children}, nil
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/tmkontra/s3-explorer
 go 1.13
 
 require (
-	github.com/aws/aws-sdk-go v1.44.57
+	github.com/aws/aws-sdk-go-v2 v1.16.7
+	github.com/aws/aws-sdk-go-v2/config v1.15.14
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.27.1
 	github.com/gin-gonic/gin v1.8.1
 	github.com/spf13/viper v1.12.0
 )
